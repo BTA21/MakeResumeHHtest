8b991036fc97af7342588219a0506407f004ca54
NabievAzamat <azamat.nabiev21@yandex.ru>
2020-02-10 16:38:23 +0500

    some changes

--- src/test/java/InputBirthDateTest.java
+++ src/test/java/InputBirthDateTest.java
@@ -1,3 +1,4 @@
+import io.qameta.allure.Step;
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -38,20 +39,21 @@ public class InputBirthDateTest {
         }
         driver.quit();
     }
-
+    
+    @Step("Вводим Test день рождения, чтобы проверить появление ошибки некорректной даты")
     @Test
     public void typeBirthdayDateDayTest1() throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", "Test", "09", 1996, "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", "Test", "09", 1996, "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Некорректная дата']")).size() > 0;
         Boolean expected = true;
         assertEquals(expected,actual);
     }
-
+    @Step("Вводим 123 день рождения, чтобы проверить появление ошибки некорректной даты")
     @Test
     public void typeBirthdayDateDayTest2() throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", 123, "09", 1996, "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", 123, "09", 1996, "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Некорректная дата']")).size() > 0;
@@ -59,9 +61,10 @@ public class InputBirthDateTest {
         assertEquals(expected,actual);
     }
 
+    @Step("Вводим Test12 день рождения, чтобы проверить появление ошибки некорректной даты")
     @Test
     public void typeBirthdayDateDayTest3() throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", "Test12", "09", 1996, "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", "Test12", "09", 1996, "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Некорректная дата']")).size() > 0;
@@ -69,9 +72,10 @@ public class InputBirthDateTest {
         assertEquals(expected,actual);
     }
 
+    @Step("Вводим корректную дату рождения, чтобы проверить отсутствие ошибки некорректной даты")
     @Test
     public void typeBirthdayDateDayTest4() throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", 12, "09", 1996, "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", 12, "09", 1996, "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Некорректная дата']")).size() > 0;
@@ -79,9 +83,10 @@ public class InputBirthDateTest {
         assertEquals(expected,actual);
     }
 
+    @Step("Пропускаем ввод месяца рождения, чтобы проверить появление ошибки некорректной даты")
     @Test
     public void typeBirthdayDateMonthTest1()  throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", 12, 1996, "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", 12, 1996, "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Некорректная дата']")).size() > 0;
@@ -89,9 +94,10 @@ public class InputBirthDateTest {
         assertEquals(expected,actual);
     }
 
+    @Step("Вводим Test год рождения, чтобы проверить появление ошибки некорректной даты")
     @Test
     public void typeBirthdayDateYearTest1() throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", 12, "09", "Test", "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", 12, "09", "Test", "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Некорректная дата']")).size() > 0;
@@ -99,29 +105,30 @@ public class InputBirthDateTest {
         assertEquals(expected,actual);
     }
 
+    @Step("Вводим Test12 год рождения, чтобы проверить появление ошибки некорректной даты")
     @Test
     public void typeBirthdayDateYearTest2() throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", 12, "09", "Test12", "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", 12, "09", "Test12", "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Некорректная дата']")).size() > 0;
         Boolean expected = true;
         assertEquals(expected,actual);
     }
-
+    @Step("Вводим 12 год рождения, чтобы проверить появление ошибки некорректной даты")
     @Test
     public void typeBirthdayDateYearTest3() throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", 12, "09", 12, "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", 12, "09", 12, "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Некорректная дата']")).size() > 0;
         Boolean expected = true;
         assertEquals(expected,actual);
     }
-
+    @Step("Вводим 1899 год рождения, чтобы проверить появление ошибки возраста")
     @Test
     public void typeBirthdayDateYearTest4() throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", 12, "09", 1899, "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", 12, "09", 1899, "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Слишком рано']")).size() > 0;
@@ -129,9 +136,10 @@ public class InputBirthDateTest {
         assertEquals(expected,actual);
     }
 
+    @Step("Вводим 2006 год рождения, чтобы проверить появление ошибки возраста")
     @Test
     public void typeBirthdayDateYearTest5() throws IOException, ParseException {
-        makeResumeNow.doIt("Азамат", "Набиев", "Уфа", 12, "09", 2006, "Мужской", "Нет опыта работы");
+        makeResumeNow.doIt("Иван", "Иванов", "Омск", 12, "09", 2006, "Мужской", "Нет опыта работы");
         makeResumeNow.submitPushResume();
         waitTime(timeForPause);
         Boolean actual = driver.findElements(By.xpath("//div [@class='bloko-form-error bloko-form-error_entered' and text()= 'Вы слишком молоды']")).size() > 0;
